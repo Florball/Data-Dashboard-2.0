19c59d7cb76812733f74d29a8fdd5a11e2b139a2
Flor Ballinas <flor_ballinas@hotmail.com>
2018-12-22 22:57:20 -0500

    funcion para pintar cohorts, implementada

--- output/main.js
+++ output/main.js
@@ -2,48 +2,61 @@
 
 // Accediendo a elementos del DOM
 var btnLima = document.getElementById('btn-cohorts');
-var listCohorts = document.getElementById('cohorts'); // Data cohorts - progress - users
+var listCohorts = document.getElementById('cohorts'); // Data cohorts - users - progress
 
-var cohorts = '../data/cohorts/cohorts.json'; // Evento muestra cohorts de sede LIMA
+var cohorts = '../data/cohorts/cohorts.json';
+var users = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+var Progress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json'; // Evento muestra cohorts de sede LIMA
 
 btnLima.addEventListener('click', function () {
-  return getData(cohorts);
+  return getData(cohorts, getCohort);
 }); // Solicitando datos
 
-var getData = function getData(url) {
+var getData = function getData(url, callback) {
   var xhttp = new XMLHttpRequest();
   xhttp.open('GET', url, true);
   xhttp.send();
 
-  xhttp.onload = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      var data = JSON.parse(this.responseText); // console.log(data);
+  xhttp.onerror = function () {
+    return console.log('Se ha producido un error durante la transacción.');
+  };
 
-      listCohorts.innerHTML = '';
-      var _iteratorNormalCompletion = true;
-      var _didIteratorError = false;
-      var _iteratorError = undefined;
+  xhttp.onload = callback;
+}; // Función para pintar los datos
 
+
+function getCohort() {
+  var self = this;
+
+  if (self.readyState === 4 && self.status === 200) {
+    var data = JSON.parse(self.responseText); // console.log(data);
+
+    listCohorts.innerHTML = '';
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = data[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var item = _step.value;
+        // console.log(item.id);
+        listCohorts.innerHTML += "\n        <tr>\n          <td id=\"".concat(item.id, "\" class=\"list-cohort\">").concat(item.id, "</td>\n          <br><br>\n        </tr>\n      ");
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
       try {
-        for (var _iterator = data[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-          var cohort = _step.value;
-          // console.log(cohort.id);
-          listCohorts.innerHTML += "\n        <tr>\n          <td>".concat(cohort.id, "</td>\n          <br>\n        </tr>\n        ");
+        if (!_iteratorNormalCompletion && _iterator.return != null) {
+          _iterator.return();
         }
-      } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
       } finally {
-        try {
-          if (!_iteratorNormalCompletion && _iterator.return != null) {
-            _iterator.return();
-          }
-        } finally {
-          if (_didIteratorError) {
-            throw _iteratorError;
-          }
+        if (_didIteratorError) {
+          throw _iteratorError;
         }
       }
     }
-  };
-};
+  }
+}
+
+;
--- src/main.js
+++ src/main.js
@@ -1,33 +1,40 @@
 // Accediendo a elementos del DOM
 const btnLima = document.getElementById('btn-cohorts');
-const listCohorts = document.getElementById('cohorts')
+const listCohorts = document.getElementById('cohorts');
 
-// Data cohorts - progress - users
+// Data cohorts - users - progress
 const cohorts = '../data/cohorts/cohorts.json';
+const users = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+const Progress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 
 // Evento muestra cohorts de sede LIMA
-btnLima.addEventListener('click', () => getData(cohorts));
+btnLima.addEventListener('click', () => getData(cohorts, getCohort));
 
 // Solicitando datos
-const getData = (url) => {
+const getData = (url, callback) => {
   const xhttp = new XMLHttpRequest();
   xhttp.open('GET', url, true);
   xhttp.send();
-  xhttp.onload = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      let data = JSON.parse(this.responseText);
-      // console.log(data);
-      listCohorts.innerHTML = '';
+  xhttp.onerror = () => console.log('Se ha producido un error durante la transacción.');
+  xhttp.onload = callback;
+};
 
-      for (let cohort of data) {
-        // console.log(cohort.id);
-        listCohorts.innerHTML += `
+// Función para pintar los datos
+function getCohort() {
+  let self = this;
+  if (self.readyState === 4 && self.status === 200) {
+    let data = JSON.parse(self.responseText);
+    // console.log(data);
+    listCohorts.innerHTML = '';
+
+    for (let item of data) {
+      // console.log(item.id);
+      listCohorts.innerHTML += `
         <tr>
-          <td>${cohort.id}</td>
-          <br>
+          <td id="${item.id}" class="list-cohort">${item.id}</td>
+          <br><br>
         </tr>
-        `
-      }
+      `
     }
-  };
+  }
 };
